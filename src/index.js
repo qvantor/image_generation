bb2cdc6864fed2a27cb0b3535104bf87bc0ad2eb
SergNikolaev <iintersergey@gmail.com>
2016-11-25 01:04:01 +0200

    v2

--- src/index.js
+++ src/index.js
@@ -20,34 +20,42 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     createMain().then(function (mainCanvas) {
-        setTimeout(function () {
-            var testCanvas = document.getElementById('test'),
-                mostColors = getMostUsedColors(mainCanvas),
-                lastScore = 0;
+        var testCanvas = document.getElementById('test'),
+            lastScore = 0, i = 0;
+        setInterval(function () {
+            var mutation = makeMutation(testCanvas, mainCanvas);
+            // if (mutation.score > lastScore) {
+                console.log(mutation);
+                drawTriangle(testCanvas, mutation.shape, mutation.color);
+                lastScore = mutation.score;
+            // }
+        }, 2);
 
-            for (var j = 0; j < 20; j++) {
-                var mutations = [];
 
-                for (var i = 0; i < 3; i++) {
-                    mutations.push(makeMutation(testCanvas, mostColors, mainCanvas));
-                }
-                if (lastScore < mutations[0].score) {
-                    mutations.sort(function (a, b) {
-                        if (a.score > b.score) {
-                            return -1;
-                        } else if (a.score < b.score) {
-                            return 1;
-                        } else {
-                            return 0;
-                        }
-                    });
-
-                    console.log(mutations[0].score);
-                    drawTriangle(testCanvas, mutations[0].mutation.triangle, mutations[0].mutation.color);
-                }
-            }
-        }, 500);
     });
+    
+    function getColorUnderShape(mainCanvas, tri) {
+        var RGB = mainCanvas
+            .getContext('2d')
+            .getImageData(tri[0].x + (tri[1].x/2), tri[0].y + (tri[1].x/2), 1, 1)
+            .data;
+        return 'rgb(' + [RGB[0],RGB[1],RGB[2]].join(',') +')';
+    }
+
+    function makeMutation(canvas, mainCanvas) {
+        var newCanvas = document.createElement('canvas');
+        var context = newCanvas.getContext('2d');
+        newCanvas.width = canvas.width;
+        newCanvas.height = canvas.height;
+        context.drawImage(newCanvas, 0, 0);
+        var tr = createTriangle(newCanvas),
+            color = getColorUnderShape(mainCanvas, tr.coords);
+        drawTriangle(newCanvas, tr.shape, color);
+        var myNode = document.getElementById("tt");
+        myNode.innerHTML = '';
+        myNode.appendChild(newCanvas);
+        return {score: compareTwoImages(mainCanvas, newCanvas), shape: tr.shape, color: color};
+    }
 
 
     function compareTwoImages(first, second) {
@@ -59,14 +67,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 .getContext('2d')
                 .getImageData(0, 0, second.width, second.height)
                 .data,
-            onePixelSame = 100 / (firstRGB.length / 16),
+            onePixelSame = 100 / (firstRGB.length / 4),
             sameOn = 0;
-        for (var i = 0; i < firstRGB.length; i += 16) {
+        for (var i = 0; i < firstRGB.length; i += 4) {
             if (
                 lI(firstRGB[i]) === lI(secondRGB[i]) &&
                 lI(firstRGB[i + 1]) === lI(secondRGB[i + 1]) &&
-                lI(firstRGB[i + 2]) === lI(secondRGB[i + 2]) &&
-                lI(firstRGB[i + 3]) === lI(secondRGB[i + 3])) {
+                lI(firstRGB[i + 2]) === lI(secondRGB[i + 2])
+            ) {
                 sameOn += onePixelSame;
             }
         }
@@ -74,71 +82,26 @@ document.addEventListener("DOMContentLoaded", function () {
         return sameOn;
     }
 
-    function getMostUsedColors(canvas) {
-        var RGBA = canvas
-                .getContext('2d')
-                .getImageData(0, 0, canvas.width, canvas.height)
-                .data,
-            pixelsCount = (RGBA.length / 4) * .01;
-
-        var colors = {};
-        var colorsArr = [];
-
-        for (var i = 0; i < RGBA.length; i += 4) {
-            var now = [lI(RGBA[i]), lI(RGBA[i + 1]), lI(RGBA[i + 2])].join(',');
-            colors[now] ? colors[now]++ : colors[now] = 1;
-        }
-
-        Object.keys(colors).forEach(function (item) {
-            if (colors[item] >= pixelsCount) {
-                colorsArr.push(item);
-            }
-        });
-
-        return colorsArr;
-    }
-
     function lI(number) {
-        return (number / 10).toFixed() * 10;
+        return number;
     }
 
     function drawTriangle(canvas, shape, color) {
         var ctx = canvas.getContext('2d');
-
-        ctx.fillStyle = 'rgb(' + color + ')';
+        ctx.fillStyle = color;
         ctx.fill(shape);
-        // ctx.fillRect(randomBTW(canvas.width), randomBTW(canvas.height), randomBTW(canvas.width), randomBTW(canvas.width));
     }
 
     function createTriangle(canvas) {
         var shape = new Path2D();
-        shape.moveTo(randomBTW(canvas.width), randomBTW(canvas.height));
-        shape.lineTo(randomBTW(canvas.width), randomBTW(canvas.height));
-        shape.lineTo(randomBTW(canvas.width), randomBTW(canvas.height));
-        return shape;
-    }
-
-    function makeMutation(oldCanvas, colors, mainCanvas) {
-        var newCanvas = document.createElement('canvas');
-        var context = newCanvas.getContext('2d');
-        newCanvas.width = oldCanvas.width;
-        newCanvas.height = oldCanvas.height;
-        context.drawImage(oldCanvas, 0, 0);
-
-        var triangle = createTriangle(newCanvas),
-            colorMutations = {},
-            bestM = 0;
-        colors.forEach(function (color) {
-            drawTriangle(newCanvas, triangle, color);
-            colorMutations[compareTwoImages(mainCanvas, newCanvas)] = {triangle: triangle, color: color};
-            // console.log(compareTwoImages(mainCanvas, newCanvas));
-        });
-        Object.keys(colorMutations).forEach(function (item) {
-            if (bestM < item) {
-                bestM = item;
-            }
-        });
-        return {mutation: colorMutations[bestM], score: bestM};
+        var coords = [{x: randomBTW(canvas.width), y: randomBTW(canvas.height)},
+            {x: randomBTW(10), y:randomBTW(10)},
+            {x: randomBTW(canvas.width), y:randomBTW(canvas.height)}];
+        // shape.moveTo(coords[0].x, coords[0].y);
+        // shape.lineTo(coords[1].x, coords[1].y);
+        // shape.lineTo(coords[2].x, coords[2].y);
+        shape.rect(coords[0].x, coords[0].y,coords[1].x, coords[1].y);
+        return {shape: shape, coords: coords};
     }
 
     function randomBTW(btw) {
